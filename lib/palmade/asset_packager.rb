fea6f7c8f01bf508c3227356997d94318ed77fd5
mark <mark@e1a20a09-3723-0410-b39d-96994b2a870a>
2007-11-22 10:06:46 +0000

    git-svn-id: https://svn.caresharing.eu/gems/asset_packager@2887 e1a20a09-3723-0410-b39d-96994b2a870a

--- lib/palmade/asset_packager.rb
+++ lib/palmade/asset_packager.rb
@@ -37,7 +37,7 @@ require File.join(File.dirname(__FILE__), 'asset_packager/package')
 require File.join(File.dirname(__FILE__), 'asset_packager/parser')
 require File.join(File.dirname(__FILE__), 'asset_packager/manager')
 
-if defined?(ActionController) && defined?(ActionView) && defined?(RAILS_ROOT)
+if defined?(RAILS_ROOT)
   # ok, add RAILS extensions
   require File.join(File.dirname(__FILE__), 'rails_packager')
   require File.join(File.dirname(__FILE__), 'extend/action_controller/base')
